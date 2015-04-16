40166832c85f23091f70b89cc7ff85c1c8fa1db7
xinglongjian <xinglongjian@qq.com>
2015-04-16 22:02:01 +0800

    update

--- js/example.js
+++ js/example.js
@@ -8,5 +8,20 @@ $(document).ready(function(){
 	context.strokeStyle='blue';
 	context.fillText('Hello Canvas',canvas.width/2-150,canvas.height/2+15);
 	context.strokeText('Hello Canvas',canvas.width/2-150,canvas.height/2+15);
+	
+	//圆角
+	//context.lineJoin='round';
+	//context.lineWidth=10;
+	//context.strokeRect(10,20,50,50);
+	
+	context.beginPath();
+	context.rect(10,10,100,100);
+	context.stroke();
+	//context.beginPath();
+	context.rect(70,70,100,100);
+	context.stroke();
+	context.rect(90,90,100,100);
+	context.stroke();
+	
 	console.profileEnd();
 });
