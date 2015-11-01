f50cdcc8d8b13471ed4197b25a07649dcf04fbe8
xinglongjian <xinglongjian@qq.com>
2015-11-01 19:31:33 +0800

    update

--- nodejsweb/index.js
+++ nodejsweb/index.js
@@ -4,14 +4,20 @@
 var express = require('express');
 var app =express();
 
+//config the views
+app.set('views','./views');
+app.set('view engine','jade');
+
+
 app.get('/',function(req,res){
-	res.send('hello world');
+	//res.send('hello world');
+	res.render('index',{title:'Hey',message:'hello there!'});
 });
 
 
+
+
 var server = app.listen(3000,function(){
-	console.log(typeof(server));
-	console.log(typeof(server.address()));
 	var host=server.address().address;
 	var port=server.address().port;
 	
---
+++ nodejsweb/lib/server/server.js
@@ -0,0 +1,21 @@
+/**
+ * create nodejsweb base on express4.13.3
+ */
+var express = require('express');
+var app =express();
+
+app.get('/',function(req,res){
+	res.send('hello world');
+});
+
+
+var server = app.listen(3000,function(){
+	console.log(typeof(server));
+	console.log(typeof(server.address()));
+	var host=server.address().address;
+	var port=server.address().port;
+	
+	console.log('Example app listening at http://%s:%s',host,port);
+});
+
+exports.server=server;
